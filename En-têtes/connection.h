bef7e285a910e83ca443a3ed28783c7f896d2a28
Mohamed <mohamedzarrouk220@gmail.com>
2026-02-24 08:45:02 +0100

    Update connection.h
    Removed singleton pattern methods and adjusted constructor.

--- En-têtes/connection.h
+++ En-têtes/connection.h
@@ -8,27 +8,11 @@
 class Connection
 {
 public:
-    // Méthode statique pour obtenir l'instance unique (Singleton)
-    static Connection& getInstance();
-
-    // Méthode pour créer la connexion
-    bool createConnection();
-
-    // Méthode pour obtenir la base de données
+    Connection();
+    bool createconnect();
     QSqlDatabase getDatabase();
 
-    // Supprimer le constructeur de copie et l'opérateur d'affectation
-    Connection(const Connection&) = delete;
-    Connection& operator=(const Connection&) = delete;
-
 private:
-    // Constructeur privé
-    Connection();
-
-    // Destructeur privé
-    ~Connection();
-
-    // Instance de la base de données
     QSqlDatabase db;
 };
 
